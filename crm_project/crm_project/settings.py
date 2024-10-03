de6b00fd3fc838875df4f88af3e09af03861c78f
dipaygi <tlyapov@bk.ru>
2024-10-03 23:06:25 +0500

    v 0.0.2

--- crm_project/crm_project/settings.py
+++ crm_project/crm_project/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'crm_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'w3-django-project.cdxmgq9zqqlr.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
